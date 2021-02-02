13d21e0d73e4e84045c6f4b160cbe9c57cae0b0f
Pedro Pacheco <pacheckdev@gmail.com>
2021-02-02 17:13:32 -0300

    commit test

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
 
-//CUCK
+//Client ID
+// vDrul5U7nWnxU4nyAxxfdQ
+
+// API Key
+// SszBEEYGNE5exDcOR3_5m23Wchw2Nb3vQrl9XaDUXFTeAMD-WB4PavqH481hYuGTu6rRtizWyku6owy2uxu6arIFGKdDJD0qOV_ITwTSkhxnpjzdYU7csNgP254ZYHYx
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+
+const Home = () => {
+    return <Text>Caralho</Text>
+}
+
+export default Home
