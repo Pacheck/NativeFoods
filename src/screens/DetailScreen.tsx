f6e12c128d5fecf74a781e89474f6fd76b7e94a6
Pedro Pacheco <pacheckdev@gmail.com>
2021-02-16 19:00:58 -0300

    Added de data to Details, and the fetch logic from api to render data

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
 
-const DetailScreen = () => {
-    return <Text>Details!</Text>
-}
+import yelpi from '../api/yelpi';
 
-const styles = StyleSheet.create({})
+import { IDetailNavProp, IResult } from './type';
+
+const DetailScreen = ({ navigation }: IDetailNavProp) => {
+  const [result, setResult] = useState<IResult>();
+  const id = navigation.getParam('id');
+
+  const getResult = async (id: string) => {
+    const response = await yelpi.get(`/${id}`);
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
+  if (!result) {
+    return null;
+  }
+
+  return (
+    <View>
+      <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={({ item }) => {
+          return <Image style={styles.Image} source={{ uri: item }} />;
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  Image: {
+    width: 300,
+    height: 200,
+  },
+});
 
 export default DetailScreen;
