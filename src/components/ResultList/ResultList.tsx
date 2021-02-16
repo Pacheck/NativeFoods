4ceb4c13db6fa6ada346da0e8262e6296b7e847f
Pedro Pacheco <pacheckdev@gmail.com>
2021-02-16 18:59:36 -0300

    Added HOF to pass navigate to the childrens and types

--- src/components/ResultList/ResultList.tsx
+++ src/components/ResultList/ResultList.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
-import {View, Text, StyleSheet, FlatList} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
 
+import { withNavigation } from 'react-navigation';
+
+import ResultCard from '../ResultCard';
 import { ITitle } from './type';
 
-import ResultCard from "../ResultCard";
+const ResultList = ({ title, results, navigation }: ITitle) => {
+  if (!results.length) {
+    return null;
+  }
 
-const ResultList = ({ title, results }: ITitle) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
       <FlatList
-        horizontal 
+        showsHorizontalScrollIndicator={false}
+        horizontal
         data={results}
-        keyExtractor={result => result.id}
-        renderItem={({ item }) => <ResultCard result={item} />}
+        keyExtractor={(result) => result.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate('Details', { id: item.id })}
+          >
+            <ResultCard result={item} />
+          </TouchableOpacity>
+        )}
       />
     </View>
   );
@@ -23,7 +41,12 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 18,
     fontWeight: 'bold',
+    marginLeft: 15,
+    marginBottom: 5,
+  },
+  container: {
+    marginBottom: 10,
   },
 });
 
-export default ResultList;
+export default withNavigation(ResultList);
--- src/components/ResultList/type.ts
+++ src/components/ResultList/type.ts
@@ -1,4 +1,8 @@
+import { StackNavigationProp } from "react-navigation-stack/lib/typescript/src/vendor/types";
+
+
 export interface ITitle {
     title: string;
     results: any;
-  }
+    navigation: StackNavigationProp;
+}
