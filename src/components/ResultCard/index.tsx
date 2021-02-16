8d7dcbfac0bfb980e5f03bb489f0c65b1566adea
Pedro Pacheco <pacheckdev@gmail.com>
2021-02-16 18:58:48 -0300

    created ResultCard and types to show the data

--- src/components/ResultCard/index.tsx
+++ src/components/ResultCard/index.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
 
-import { View, Text, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import {HeaderHeightContext} from 'react-navigation-stack';
 
-import { ICardProps } from './type';
+import {ICardProps} from './type';
 
 //Stephen used 'ResultDetail' i named the function as ResultCard
-const ResultCard = ({ result }: ICardProps) => {
+const ResultCard = ({result}: ICardProps) => {
   return (
-    <View>
-      <Text>{result.name}</Text>
+    <View style={styles.container}>
+      <Image style={styles.cardImage} source={{uri: result.image_url}} />
+      <Text style={styles.cardName}>{result.name}</Text>
+      <Text>{result.rating} Stars, {result.review_count} Reviews.</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  cardImage: {
+    width: 250,
+    height: 120,
+    borderRadius: 4,
+    marginBottom: 5,
+  },
+  cardName: {
+    fontWeight: 'bold',
+  },
+  container: {
+    marginLeft: 15,
+  }
+});
 
 export default ResultCard;
--- src/components/ResultCard/type.ts
+++ src/components/ResultCard/type.ts
@@ -1,5 +1,8 @@
 export interface ICardProps {
     result: {
         name: string;
+        image_url: string;
+        rating: number;
+        review_count: number;
     }
 }
