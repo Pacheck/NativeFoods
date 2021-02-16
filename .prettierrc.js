89afe46ba2264471d824c9efe957eafc43203fc0
Pedro Pacheco <pacheckdev@gmail.com>
2021-02-16 18:56:13 -0300

    config prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,8 @@
 module.exports = {
-  bracketSpacing: false,
+  bracketSpacing: true,
   jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
+  jsxSingleQuote: false,
+  jsxBracketSameLine: false,
 };
