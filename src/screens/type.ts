10e0c31b96432b6e8495aeeab6b9c7b3a79c7efc
Pedro Pacheco <pacheckdev@gmail.com>
2021-02-16 19:02:54 -0300

    Added new types to components

---
+++ src/screens/type.ts
@@ -0,0 +1,10 @@
+import { NavigationStackProp } from "react-navigation-stack";
+
+export interface IDetailNavProp {
+    navigation: NavigationStackProp
+}
+
+export interface IResult {
+    name: string;
+    photos: string[];
+};
